57a0601918ca30dcc45a77101893d2f0d718c90f
a305519 <a305519@NBR660925.rio.ar.bsch>
2018-09-10 11:12:36 -0300

    .

--- Servidor/src/servidor/Server.java
+++ Servidor/src/servidor/Server.java
@@ -5,6 +5,8 @@ import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 
+import javax.swing.JOptionPane;
+
 import interfaces.InterfaceRemota;
 import remoto.ObjetoRemoto;
 
@@ -22,6 +24,7 @@ public class Server {
 		try {
 			LocateRegistry.createRegistry(1099);
 			Naming.rebind("//127.0.0.1/sumador", or);
+			System.out.println("Servidor iniciado Correctamente!");
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
