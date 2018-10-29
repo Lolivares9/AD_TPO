315d5ac871da796b5a911a3a14d6c38b47dd2596
l0675806 <l0675806@GAL128461.bgcmz.bancogalicia.com.ar>
2018-10-29 17:39:02 -0300

    Test

--- Servidor/src/test/TestHibernate.java
+++ Servidor/src/test/TestHibernate.java
@@ -3,11 +3,16 @@ package test;
 import controlador.Controlador;
 import dao.JugadorDAO;
 import dao.ParejaDAO;
+import dto.PartidoDTO;
+import enums.Categoria;
+import excepciones.CartaException;
 import excepciones.JugadorException;
 import excepciones.ParejaException;
+import excepciones.PartidoException;
 import negocio.Jugador;
 import negocio.Pareja;
 import negocio.Partido;
+import util.DTOMapper;
 
 public class TestHibernate {
 /*
@@ -16,14 +21,26 @@ public class TestHibernate {
 	DBCC CHECKIDENT ('APD.dbo.PAREJAS',RESEED, 0)
 	*/
 	
-	public static void main(String[] args) throws JugadorException, ParejaException {
+	public static void main(String[] args) throws JugadorException, ParejaException, PartidoException, CartaException {
+		Partido partido = null;
 		//guardarParejas();
 		//guardarPartido();
 		//iniciarPartidaLibre();
+		partido = iniciarPartidaLibreIndividual();
+		
+	}
+
+	private static Partido iniciarPartidaLibreIndividual() throws PartidoException, CartaException, JugadorException {
+		Jugador jug = JugadorDAO.getInstancia().buscarPorApodo("Mati");
+		Partido part = null;
+		Categoria categ = jug.getCategoria();
+		PartidoDTO partido = Controlador.getInstancia().iniciarPartidaLibreIndividual(categ, jug);
+		part = DTOMapper.getInstancia().partidoDTOtoNegocio(partido);
+		return part;
 	}
 
 	private static void iniciarPartidaLibre() throws ParejaException, JugadorException {
-		Jugador jug = JugadorDAO.getInstancia().buscarPorApodo("Chulo");
+		Jugador jug = JugadorDAO.getInstancia().buscarPorApodo("Mati");
 		Jugador jug2 = JugadorDAO.getInstancia().buscarPorApodo("Kent");
 		Pareja uno = new Pareja(jug,jug2);
 		uno.setIdPareja(1);
